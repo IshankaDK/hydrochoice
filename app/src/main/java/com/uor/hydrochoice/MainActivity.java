c47b0a3fa126a3f2b8894356a1f4b2ab5bc01b19
IshankaDK <idkodippili@gmail.com>
2022-05-06 12:36:24 +0530

    signup page ui done and navigation to login page

--- app/src/main/java/com/uor/hydrochoice/MainActivity.java
+++ app/src/main/java/com/uor/hydrochoice/MainActivity.java
@@ -6,35 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
 
+    TextView linkNewAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogin = findViewById(R.id.loginButton);
-        btnLogin.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent1 = new Intent(MainActivity.this, SignUpActivity.class);
-                        startActivity(intent1);
-                    }
+        linkNewAccount = findViewById(R.id.linkNewAccount);
+        linkNewAccount.setOnClickListener(
+                view -> {
+                    Intent intent1 = new Intent(MainActivity.this, SignUpActivity.class);
+                    startActivity(intent1);
                 }
         );
 
     }
 
-//    public void btnLoginAction(View view){
-//        Toast.makeText(getApplicationContext(),"Login button clicked",Toast.LENGTH_SHORT).show();
-//    }
 
-    public void txtNewAccAction(View view){
-        Toast.makeText(getApplicationContext(),"New Account button clicked",Toast.LENGTH_SHORT).show();
-    }
 }
--- app/src/main/java/com/uor/hydrochoice/SignUpActivity.java
+++ app/src/main/java/com/uor/hydrochoice/SignUpActivity.java
@@ -6,24 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
     Button btnSignUp;
+    TextView linkLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        btnSignUp = findViewById(R.id.loginButton);
-        btnSignUp.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent1 = new Intent(SignUpActivity.this, MainActivity.class);
-                        startActivity(intent1);
-                    }
+        linkLogin = findViewById(R.id.linkLogin);
+        linkLogin.setOnClickListener(
+                view -> {
+                    Intent intent1 = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent1);
                 }
         );
     }
